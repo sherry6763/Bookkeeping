b42173ced33d1997f256a3eb86ce0116c64f0876
sherryma <sherry6763@hotmail.com>
2017-08-08 14:06:44 +0800

    add

---
+++ Bookkeeping/Bookkeeping/Controllers/TallyController.cs
@@ -0,0 +1,43 @@
+﻿using Bookkeeping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookkeeping.Controllers
+{
+    public class TallyController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View(new TallyRecord());
+        }
+
+
+        [ChildActionOnly]
+        public ActionResult Detail()
+        {
+
+            var model = new List<TallyRecord>();
+            var count = 10;
+            Random rnd = new Random();
+
+            for (var i =1; i <= count; i ++)
+            {
+                model.Add(new TallyRecord()
+                {
+                    No = i,
+                    Category = (i % 3 == 0) ? EnumTypes.支出 : EnumTypes.收入,
+                    Date = DateTime.Now.Date.AddDays(count - i),
+                    Money = rnd.Next(100, 2000)
+                });
+            }
+            return View(model);
+        }
+
+
+
+    }
+}
---
+++ Bookkeeping/Bookkeeping/Models/TallyViewModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookkeeping.Models
+{
+    public class TallyRecord
+    {
+        [Display(Name = "#")]
+        public int No { get; set; }
+
+        [Display(Name = "類別")]
+        public EnumTypes Category { get; set; }
+
+        [Display(Name = "金額")]
+        public int? Money { get; set; }
+
+        [Display(Name = "日期")]
+        public DateTime? Date { get; set; }
+
+        [Display(Name = "備註")]
+        public string Description { get; set; }
+    }
+
+    public enum EnumTypes
+    {
+        收入 = 1,
+        支出 = 2
+    }
+}
